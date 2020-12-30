5cfe6a5ff886d030e4656d43e20259dc4e8822b3
Nomuken <nomuken@spica.bz>
2020-12-31 03:48:17 +0900

    fix lint rule violations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { convertFromVersionSlug, downloadPackerCLI, renamePackerCLI } from './main'
+import {convertFromVersionSlug, downloadPackerCLI} from './main'
 import * as core from '@actions/core'
 
 async function runner(): Promise<void> {
-  const target_version = await convertFromVersionSlug(core.getInput("packer_version"))
+  const target_version = await convertFromVersionSlug(
+    core.getInput('packer_version')
+  )
   const exec_dir_path = await downloadPackerCLI(target_version)
 
   // not planning to implement wrapper function ;P
   // await renamePackerCLI(exec_dir_path)
 
   core.addPath(exec_dir_path)
-
 }
 
-runner().catch(core.setFailed)
+try {
+  runner()
+} catch (e) {
+  core.setFailed(e)
+}
