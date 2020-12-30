9b3d073700ef4ed47e2c5a7a4c6922838fe7bf2c
Nomuken <nomuken@spica.bz>
2020-12-31 05:19:34 +0900

    runnable

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import {convertFromVersionSlug, downloadPackerCLI} from './main'
 import * as core from '@actions/core'
 
-async function runner(): Promise<void> {
-  const target_version = await convertFromVersionSlug(
-    core.getInput('packer_version')
-  )
-  const exec_dir_path = await downloadPackerCLI(target_version)
+async function run(): Promise<void> {
+  try {
+    const target_version = await convertFromVersionSlug(
+      core.getInput('packer_version')
+    )
+    const exec_dir_path = await downloadPackerCLI(target_version)
 
-  // not planning to implement wrapper function ;P
-  // await renamePackerCLI(exec_dir_path)
+    // not planning to implement wrapper function ;P
+    // await renamePackerCLI(exec_dir_path)
 
-  core.addPath(exec_dir_path)
+    core.addPath(exec_dir_path)
+  } catch (e) {
+    core.setFailed(e)
+  }
 }
 
-try {
-  console.log("tasukete")
-  runner()
-} catch (e) {
-  core.setFailed(e)
-}
+run()
